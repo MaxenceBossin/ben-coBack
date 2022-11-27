557efe6f8268cd804529db337e3ea6c5dc54d33d
MaxenceBossin <maxbossin25@gmail.com>
2022-11-27 13:16:53 +0100

    protection

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * @Route("/api", name="api_")
@@ -24,36 +25,37 @@ class UserController extends AbstractController
             EntityManagerInterface $entityManager,
             Request $request,
             UserPasswordHasherInterface $passwordHasher,
-            HttpClientInterface $httpClient
             ): JsonResponse
     {
         $data = json_decode($request->getContent());
-        $user = new User();
-        
 
+        if(!filter_var($data->email, FILTER_VALIDATE_EMAIL)){
+            return throw $this->createNotFoundException('Mauvais format email');
+        }
+        if(strlen($data->password) < 5){
+            return throw $this->createNotFoundException('Mauvais format de mots de passe');
+        }
+        return $this->json(strlen($data->password));
+        $user = new User();
         $user->setEmail($data->email);
-    
         $user->setPassword(
             $passwordHasher->hashPassword(
                 $user,
                 $data->password
             )
         );
-
         if($data->roles == null ||
         $data->roles != 'ROLE_USER' ||
         $data->roles != 'ROLE_GARBAGECOLLECTOR' ||
         $data->roles != 'ROLE_ADMIN' ||
-        !isset($data->roles)
-        ){
+        
+        !isset($data->roles)){
             $user->setRoles(["ROLE_USER"]);
         }else{
             $user->setRoles($data->role);
         } 
-
         if(isset($data->first_name)) $user->setFirstName($data->first_name);
         if(isset($data->last_name)) $user->setLastName($data->last_name);
-
         $entityManager->persist($user);
         $entityManager->flush();
 
