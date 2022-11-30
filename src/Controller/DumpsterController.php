b2a0015f36ec0a1c7dec824def5c0fec23864a40
AntoineBrevet <antoine.brevet@live.fr>
2022-11-30 12:13:43 +0100

    commit insertion de tous les types de bennes

--- src/Controller/DumpsterController.php
+++ src/Controller/DumpsterController.php
@@ -14,10 +14,12 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
  */
 class DumpsterController extends AbstractController
 {
-    // Fonction d'ajout des bennes verres
-    #[Route('/addDumpsterVerre', name: 'app_addDumpsterVerre')]
-    public function addDumpsterVerre(ManagerRegistry $doctrine, HttpClientInterface $httpClient): Response
+    // Fonction d'ajout de toutes les bennes
+    #[Route('/addDumpster', name: 'app_addDumpster')]
+    public function addDumpster(ManagerRegistry $doctrine, HttpClientInterface $httpClient): Response
     {
+        ini_set('max_execution_time', 300);
+
         $response = $httpClient->request(
             'GET',
             'https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=points-dapport-volontaire-dechets-et-moyens-techniques&q=&rows=10000&facet=commune&facet=details_source_geoloc&facet=details_source_attributaire&facet=type_flux&facet=prestataire&facet=centre_ville&facet=zone&refine.flux=R%C3%A9cup%27verre&refine.commune=Toulouse'
@@ -29,7 +31,7 @@ class DumpsterController extends AbstractController
         foreach ($benneInfo as $benneInfos) {
             $longitude = $benneInfos->geometry->coordinates[0];
             $latitude = $benneInfos->geometry->coordinates[1];
-            $type = "verre";
+            $type = $benneInfos->fields->type_flux;
             $commune = $benneInfos->fields->commune;
             $voie = (isset($benneInfos->fields->voie)) ? ($benneInfos->fields->voie) : null;
             $numeroVoie = (isset($benneInfos->fields->numero_voie)) ? ($benneInfos->fields->numero_voie) : null;
@@ -53,127 +55,6 @@ class DumpsterController extends AbstractController
 
         return $this->redirect('../api/showDumpster');
     }
-
-    // Fonction d'ajout des bennes ordures ménagères
-    #[Route('/addDumpsterOrdure', name: 'app_addDumpsterOrdure')]
-    public function addDumpsterOrdure(ManagerRegistry $doctrine, HttpClientInterface $httpClient): Response
-    {
-        $response = $httpClient->request(
-            'GET',
-            'https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=points-dapport-volontaire-dechets-et-moyens-techniques&q=&facet=commune&rows=10000&facet=details_source_geoloc&facet=details_source_attributaire&facet=type_flux&facet=prestataire&facet=centre_ville&facet=zone&refine.type_flux=ordures+m%C3%A9nag%C3%A8res&refine.details_source_attributaire=G%C3%A9o+Propret%C3%A9&refine.commune=Toulouse'
-        );
-
-        $benne = json_decode($response->getContent());
-        $benneInfo = $benne->records;
-
-        foreach ($benneInfo as $benneInfos) {
-            $longitude = $benneInfos->geometry->coordinates[0];
-            $latitude = $benneInfos->geometry->coordinates[1];
-            $type = "ordures ménagères";
-            $commune = $benneInfos->fields->commune;
-            $voie = (isset($benneInfos->fields->voie)) ? ($benneInfos->fields->voie) : null;
-            $numeroVoie = (isset($benneInfos->fields->numero_voie)) ? ($benneInfos->fields->numero_voie) : null;
-            $codePostal = (isset($benneInfos->fields->code_postal)) ? ($benneInfos->fields->code_postal) : null;
-
-            $entityManager = $doctrine->getManager();
-
-            $dumpster = new Dumpster();
-
-            $dumpster->setLongitude($longitude);
-            $dumpster->setLatitude($latitude);
-            $dumpster->setType($type);
-            $dumpster->setCapacity(0);
-            $dumpster->setCity($commune);
-            $dumpster->setStreetLabel($voie);
-            $dumpster->setStreetNumber($numeroVoie);
-            $dumpster->setPostalCode($codePostal);
-            $entityManager->persist($dumpster);
-            $entityManager->flush();
-        }
-
-        return $this->redirect('../api/showDumpster');
-    }
-
-    // Fonction d'ajout des bennes collectes sélectives
-    #[Route('/addDumpsterCollecte', name: 'app_addDumpsterCollecte')]
-    public function addDumpsterCollecte(ManagerRegistry $doctrine, HttpClientInterface $httpClient): Response
-    {
-        $response = $httpClient->request(
-            'GET',
-            'https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=points-dapport-volontaire-dechets-et-moyens-techniques&rows=10000&q=&facet=commune&facet=details_source_geoloc&facet=details_source_attributaire&facet=type_flux&facet=prestataire&facet=centre_ville&facet=zone&refine.details_source_attributaire=G%C3%A9o+Propret%C3%A9&refine.type_flux=collecte+s%C3%A9lective'
-        );
-
-        $benne = json_decode($response->getContent());
-        $benneInfo = $benne->records;
-
-        foreach ($benneInfo as $benneInfos) {
-            $longitude = $benneInfos->geometry->coordinates[0];
-            $latitude = $benneInfos->geometry->coordinates[1];
-            $type = "collecte sélective";
-            $commune = $benneInfos->fields->commune;
-            $voie = (isset($benneInfos->fields->voie)) ? ($benneInfos->fields->voie) : null;
-            $numeroVoie = (isset($benneInfos->fields->numero_voie)) ? ($benneInfos->fields->numero_voie) : null;
-            $codePostal = (isset($benneInfos->fields->code_postal)) ? ($benneInfos->fields->code_postal) : null;
-
-            $entityManager = $doctrine->getManager();
-
-            $dumpster = new Dumpster();
-
-            $dumpster->setLongitude($longitude);
-            $dumpster->setLatitude($latitude);
-            $dumpster->setType($type);
-            $dumpster->setCapacity(0);
-            $dumpster->setCity($commune);
-            $dumpster->setStreetLabel($voie);
-            $dumpster->setStreetNumber($numeroVoie);
-            $dumpster->setPostalCode($codePostal);
-            $entityManager->persist($dumpster);
-            $entityManager->flush();
-        }
-
-        return $this->redirect('../api/showDumpster');
-    }
-
-    // Fonction d'ajout des bennes textiles
-    #[Route('/addDumpsterTextile', name: 'app_addDumpsterTextile')]
-    public function addDumpsterTextile(ManagerRegistry $doctrine, HttpClientInterface $httpClient): Response
-    {
-        $response = $httpClient->request(
-            'GET',
-            'https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=points-dapport-volontaire-dechets-et-moyens-techniques&rows=10000&q=&facet=commune&facet=details_source_geoloc&facet=details_source_attributaire&facet=type_flux&facet=prestataire&facet=centre_ville&facet=zone&refine.details_source_attributaire=G%C3%A9o+Propret%C3%A9&refine.type_flux=textile'
-        );
-
-        $benne = json_decode($response->getContent());
-        $benneInfo = $benne->records;
-
-        foreach ($benneInfo as $benneInfos) {
-            $longitude = $benneInfos->geometry->coordinates[0];
-            $latitude = $benneInfos->geometry->coordinates[1];
-            $type = "textile";
-            $commune = $benneInfos->fields->commune;
-            $voie = (isset($benneInfos->fields->voie)) ? ($benneInfos->fields->voie) : null;
-            $numeroVoie = (isset($benneInfos->fields->numero_voie)) ? ($benneInfos->fields->numero_voie) : null;
-            $codePostal = (isset($benneInfos->fields->code_postal)) ? ($benneInfos->fields->code_postal) : null;
-
-            $entityManager = $doctrine->getManager();
-
-            $dumpster = new Dumpster();
-
-            $dumpster->setLongitude($longitude);
-            $dumpster->setLatitude($latitude);
-            $dumpster->setType($type);
-            $dumpster->setCapacity(0);
-            $dumpster->setCity($commune);
-            $dumpster->setStreetLabel($voie);
-            $dumpster->setStreetNumber($numeroVoie);
-            $dumpster->setPostalCode($codePostal);
-            $entityManager->persist($dumpster);
-            $entityManager->flush();
-        }
-
-        return $this->redirect('../api/showDumpster');
-    }
-
 
     // Fonction d'affichage de toutes les bennes
     #[Route('/showDumpster', name: 'app_showDumpster')]
