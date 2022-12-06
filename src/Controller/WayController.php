2365de6af9d3deaf14bbf8cb583cb0be3ac3e0f5
MaxenceBossin <maxbossin25@gmail.com>
2022-12-06 10:21:55 +0100

    Update Way

--- src/Controller/WayController.php
+++ src/Controller/WayController.php
@@ -15,40 +15,10 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class WayController extends AbstractController
 {
-    private $messageRepository;
-
-    public function __construct(MessageRepository $messageRepository)
-    {
-        $this->messageRepository = $messageRepository;
-    }
-
-    #[Route('/way', name: 'add_way', methods: ['POST'])]
-    public function addRoute(ManagerRegistry $doctrine, Request $request): JsonResponse
-    {
-        $data = json_decode($request->getContent());
-        $dateStart = new DateTimeImmutable($data->date_start);
-        $dateEnd = new DateTimeImmutable($data->date_end); 
-        $routeJson = [$data->route_json];
-        $teamJson =  [$data->team];
-        if($dateStart->getTimestamp() >= $dateEnd->getTimestamp()) {
-            return $this->json('Problème date', Response::HTTP_METHOD_NOT_ALLOWED);
-        }
-        /* TODO: vérifié qu'une date n'existe pas déjà */
-        $entityManager = $doctrine->getManager();
-
-        $route = new Way();
-        $route->setDateStart($dateStart);
-        $route->setDateEnd($dateEnd);
-        $route->setRouteJson($routeJson);
-        $route->setTeam($teamJson);
-
-        $entityManager->persist($route);
-        $entityManager->flush();
-        return $this->json(['route créé', Response::HTTP_OK]);
-    }
-
     #[Route('/ways', name: 'getWays', methods: ['GET'])]
-    public function getWays(ManagerRegistry $doctrine): JsonResponse {
+    public function getWays(
+        ManagerRegistry $doctrine
+        ): JsonResponse {
         $ways = $doctrine->getRepository(Way::class)->findAll();
         $data = [];
         foreach ($ways as $way) {
@@ -57,17 +27,17 @@ class WayController extends AbstractController
                 "team" => $way->getTeam(),
                 "date_start" => $way->getDateStart(),
                 "date_end" => $way->getDateEnd(),
-                "route_json" => $way->getRouteJson()     
+                "route_json" => $way->getRouteJson()
             ];
         }
 
         return $this->json($data);
     }
 
-
     #[Route('/way/{id}', name: 'getOneWay', methods: ['GET'])]
-    public function getWay(ManagerRegistry $doctrine, int $id): JsonResponse {
-
+    public function getWay(
+        ManagerRegistry $doctrine,
+        int $id): JsonResponse {
         $way = $doctrine->getRepository(Way::class)->find($id);
         if($way == null){
             return $this->json('Error'); 
@@ -85,8 +55,70 @@ class WayController extends AbstractController
 
     }
 
-    #[Route('/deleteWay/{id}', name: 'delete_way', methods: ['DELETE'])]
-    public function deleteWay(ManagerRegistry $doctrine, int $id): JsonResponse{
+    #[Route('/way', name: 'add_way', methods: ['POST'])]
+    public function addRoute(
+        ManagerRegistry $doctrine,
+        Request $request): JsonResponse {
+        $data = json_decode($request->getContent());
+        $dateStart = new DateTimeImmutable($data->date_start);
+        $dateEnd = new DateTimeImmutable($data->date_end); 
+        $routeJson = [$data->route_json];
+        $teamJson =  [$data->team];
+        if($dateStart->getTimestamp() >= $dateEnd->getTimestamp()) {
+            return $this->json('Problème date', Response::HTTP_METHOD_NOT_ALLOWED);
+        }
+        $entityManager = $doctrine->getManager();
+
+        $route = new Way();
+        $route->setDateStart($dateStart);
+        $route->setDateEnd($dateEnd);
+        $route->setRouteJson($routeJson);
+        $route->setTeam($teamJson);
+
+        $entityManager->persist($route);
+        $entityManager->flush();
+        return $this->json(['route créé', Response::HTTP_OK]);
+    }
+
+    #[Route('/way/{id}', name: 'update_way', methods: ['PUT'])]
+    public function editWay(
+        ManagerRegistry $doctrine, 
+        Request $request, 
+        int $id
+        ): JsonResponse{
+        /* Vérification ID */
+        $way = $doctrine->getRepository(Way::class)->find($id);
+        if($way == null){
+            return $this->json('Error'); 
+        }
+        $data = json_decode($request->getContent());
+        $entityManager = $doctrine->getManager();
+        
+        $data = json_decode($request->getContent());
+        $dateStart = new DateTimeImmutable($data->date_start);
+        $dateEnd = new DateTimeImmutable($data->date_end); 
+        $routeJson = [$data->route_json];
+        $teamJson =  [$data->team];
+        if($dateStart->getTimestamp() >= $dateEnd->getTimestamp()) {
+            return $this->json('Problème date');
+        }
+
+        $way->setDateStart($dateStart);
+        $way->setDateEnd($dateEnd);
+        $way->setRouteJson($routeJson);
+        $way->setTeam($teamJson);
+
+        $entityManager->persist($way);
+        $entityManager->flush();
+
+        return $this->json('Edition OK' );
+    }
+
+    #[Route('/way/{id}', name: 'delete_way', methods: ['DELETE'])]
+    public function deleteWay(
+        ManagerRegistry $doctrine,
+        int $id)
+        : JsonResponse{
 
         $entityManager = $doctrine->getManager();
 
--- src/Entity/Way.php
+++ src/Entity/Way.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\WayRepository;
-use Doctrine\DBAL\Types\JsonType;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: WayRepository::class)]
